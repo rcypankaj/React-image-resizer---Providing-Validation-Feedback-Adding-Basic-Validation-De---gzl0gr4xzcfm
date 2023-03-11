e5337bd93cf3e3f2b7369c185e75e5213fab26db
Pankaj <rcypankaj@gmail.com>
2023-03-11 07:57:15 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,60 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+import img from "../image.jpeg";
 
 const App = () => {
+  const [height, setHeight] = useState(320);
+  const [width, setWidth] = useState(320);
+
+  const heightChangeHandler = (event) => {
+    setHeight(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  const widthChangeHandler = (event) => {
+    setWidth(event.target.value);
+    // console.log(event.target.value);
+  };
+
   return (
     <div id="main">
-
+      <div>
+        <label htmlFor="height-slider">
+          Height-Slider
+          <input
+            type="range"
+            id="height-slider"
+            min="100"
+            max="800"
+            onChange={heightChangeHandler}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="width-slider">
+          Width-Slider
+          <input
+            type="range"
+            id="width-slider"
+            min="100"
+            max="800"
+            onChange={widthChangeHandler}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="resizable-img">
+          <img
+            src={img}
+            alt="image"
+            id="resizable-img"
+            width={`${width}px`}
+            height={`${height}px`}
+          />
+        </label>
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
